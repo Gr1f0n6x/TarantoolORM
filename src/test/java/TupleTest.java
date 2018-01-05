65fca6de1fa33fa67ed9d18a7b94aa5bc942e8e2
Nick <grifon52@gmail.com>
2018-01-05 15:23:31 +0300

    UnitTests for TarantoolTuple

--- src/test/java/TupleTest.java
+++ src/test/java/TupleTest.java
@@ -1,11 +1,12 @@
-import org.tarantool.orm.annotation.IndexField;
+import org.junit.Before;
 import org.junit.Test;
-import org.tarantool.orm.TarantoolField;
-import org.tarantool.orm.TarantoolTuple;
-import org.tarantool.orm.type.TarantoolType;
+import org.tarantool.orm.entity.TarantoolField;
+import org.tarantool.orm.entity.TarantoolTuple;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -13,16 +14,33 @@ import static org.junit.Assert.*;
  * Created by GrIfOn on 20.12.2017.
  */
 public class TupleTest {
-    @Test
-    public void testTupleFields() {
-        TarantoolTuple tuple = new MyTuple();
-        assertEquals("Tuple{[10, 20, value]}", tuple.toString());
+    @Before
+    public void setFields() {
+        TarantoolTuple.retrieveFieldMap(MyTuple.class);
     }
 
     @Test
-    public void testGetValues() {
+    public void testTupleFields() {
         TarantoolTuple tuple = new MyTuple();
-        assertEquals("[10, 20, value]", tuple.getValues().toString());
+        assertEquals("Tuple{[10, 20, value, value, {1, 2, 3, 4}, 100, 100]}", tuple.toString());
+    }
+
+    @Test
+    public void testGetTupleFields() {
+        assertEquals("{1=a, 2=b, 3=c, 4=e, 5=d, 6=f, 7=g}", TarantoolTuple.fieldMap.toString());
+    }
+
+    @Test
+    public void testGetValues() throws NoSuchFieldException, IllegalAccessException {
+        TarantoolTuple tuple = new MyTuple();
+        assertEquals("[10, 20, value, value, {1, 2, 3, 4}, 100, 100]", tuple.getValues().stream().map(x -> {
+            if(x.getClass().isArray()) {
+                return Arrays.toString((Object[]) x)
+                        .replace('[', '{')
+                        .replace(']', '}');
+            }
+            return x.toString();
+        }).collect(Collectors.toList()).toString());
     }
 
     @Test
@@ -46,18 +64,25 @@ public class TupleTest {
     @Test
     public void testFieldCount() {
         TarantoolTuple tuple = new MyTuple();
-        assertEquals(3, tuple.getFieldCount());
+        assertEquals(7, tuple.getFieldCount());
     }
 
     @Test
     public void testGetValuesForUpdate() {
         TarantoolTuple tuple = new MyTuple();
-        assertEquals("[[=, 0, 10], [=, 1, 20], [=, 2, value]]", Arrays.toString(tuple.getValuesForUpdate()));
+        assertEquals("[[=, 0, 10], [=, 1, 20], [=, 2, value], [=, 3, value], [=, 4, {1, 2, 3, 4}], [=, 5, 100], [=, 6, 100]]", Arrays.toString(tuple.getValuesForUpdate()));
     }
 
     @Test
-    public void testCostructorWithValueList() throws InstantiationException, IllegalAccessException {
-        MyTuple tuple = (MyTuple) TarantoolTuple.build(MyTuple.class, Arrays.asList(1, 2L, "valueFromList"));
-        assertEquals("[1, 2, valueFromList]", tuple.getValues().toString());
+    public void testCostructorWithValueList() throws InstantiationException, IllegalAccessException, NoSuchFieldException {
+        MyTuple tuple = (MyTuple) TarantoolTuple.build(MyTuple.class, Arrays.asList(1, 2L, "valueFromList", "newValue", new Integer[] {4,3,2,1}, 200, 200));
+        assertEquals("[1, 2, valueFromList, newValue, {4, 3, 2, 1}, 200, 200]", tuple.getValues().stream().map(x -> {
+            if(x.getClass().isArray()) {
+                return Arrays.toString((Object[]) x)
+                        .replace('[', '{')
+                        .replace(']', '}');
+            }
+            return x.toString();
+        }).collect(Collectors.toList()).toString());
     }
 }
